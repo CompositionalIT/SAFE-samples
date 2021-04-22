49875da5438a5c3932833b03bbef65fd064090b0
Akash Mair <akash@compositional-it.com>
2021-04-22 15:40:44 +0100

    Added value prop so code changes to selected option reflect in dropdown

--- Dropdown/src/Client/Index.fs
+++ Dropdown/src/Client/Index.fs
@@ -2,20 +2,20 @@ module Index
 
 open Elmish
 
-type Todo = { Id: int; Description: string }
+type Todo = { Id: string; Description: string }
 type Model = { DropDownOptions: Todo list; SelectedOption: string }
 
 type Msg =
     | SetDropDownOption of id: string
 
 let dropDownOptions =
-    [ { Id = 1; Description = "Clean desk" }
-      { Id = 2; Description = "Take out trash" }
-      { Id = 3; Description = "Walk the dog" }
-      { Id = 4; Description = "Cook dinner" } ]
+    [ { Id = "1"; Description = "Clean desk" }
+      { Id = "2"; Description = "Take out trash" }
+      { Id = "3"; Description = "Walk the dog" }
+      { Id = "4"; Description = "Cook dinner" } ]
 
 let init () : Model * Cmd<Msg> =
-    let model = { DropDownOptions = dropDownOptions; SelectedOption = ""  }
+    let model = { DropDownOptions = dropDownOptions; SelectedOption = "1"  }
     model, Cmd.none
 
 let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
@@ -27,9 +27,9 @@ open Fable.React.Props
 open Fable.React
 let view (model: Model) (dispatch: Msg -> unit) =
     Box.box' [] [
-        div [] [str (sprintf "2 Current selected value is: %s, since that is what the Value prop is on the select option" model.SelectedOption)]
+        div [] [str (sprintf "Current selected value is: %s, since that is what the Value prop is on the select option" model.SelectedOption)]
         Select.select [ ]
-            [ select [ DefaultValue model.SelectedOption; OnChange (fun x  -> dispatch (SetDropDownOption x.Value)) ]
+            [ select [ Value model.SelectedOption; OnChange (fun x  -> dispatch (SetDropDownOption x.Value)) ]
                 [ for row in model.DropDownOptions do
                     option [ Value row.Id ] [ str row.Description ] ] ]
     ]
