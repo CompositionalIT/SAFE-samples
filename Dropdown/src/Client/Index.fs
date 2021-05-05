1396228d5b7c6673769696d4ed4085a9c1b9da21
isaacabraham <isaac.abraham@gmail.com>
2021-05-05 18:40:46 +0200

    Slight modifications

--- Dropdown/src/Client/Index.fs
+++ Dropdown/src/Client/Index.fs
@@ -3,10 +3,11 @@ module Index
 open Elmish
 
 type Todo = { Id: string; Description: string }
-type Model = { DropDownOptions: Todo list; SelectedOption: string }
+type Model = { DropDownOptions: Todo list; SelectedOption: Todo option }
 
 type Msg =
-    | SetDropDownOption of id: string
+    | SetDropDownOption of Todo
+    | LoadedDropDown of Todo list
 
 let dropDownOptions =
     [ { Id = "1"; Description = "Clean desk" }
@@ -14,24 +15,47 @@ let dropDownOptions =
       { Id = "3"; Description = "Walk the dog" }
       { Id = "4"; Description = "Cook dinner" } ]
 
+let loadDropDownData () = async {
+    do! Async.Sleep 5000
+    return dropDownOptions
+}
+
 let init () : Model * Cmd<Msg> =
-    let model = { DropDownOptions = dropDownOptions; SelectedOption = "1"  }
-    model, Cmd.none
+    let model = { DropDownOptions = []; SelectedOption = None }
+    model, Cmd.OfAsync.perform loadDropDownData () LoadedDropDown
 
 let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
     match msg with
-    | SetDropDownOption id -> { model with SelectedOption = id }, Cmd.none
+    | SetDropDownOption id -> { model with SelectedOption = Some id }, Cmd.none
+    | LoadedDropDown data -> { model with DropDownOptions = data; SelectedOption = List.tryHead data }, Cmd.none
 
-open Fulma
-open Fable.React.Props
+open Fable.Core.JsInterop
 open Fable.React
+open Fable.React.Props
+open Fulma
+
 let view (model: Model) (dispatch: Msg -> unit) =
-    Box.box' [] [
-        div [] [str (sprintf "Current selected value is: %s, since that is what the Value prop is on the select option" model.SelectedOption)]
-        Select.select [ ]
-            [ select [ Value model.SelectedOption; OnChange (fun x  -> dispatch (SetDropDownOption x.Value)) ]
-                [ for row in model.DropDownOptions do
-                    option [ Value row.Id ] [ str row.Description ] ] ]
+    Section.section [] [
+
+        match model.DropDownOptions with
+        | [] -> str "Loading data!"
+        | _ -> ()
+
+        div [] [
+            match model.SelectedOption with
+            | Some selected -> str $"Current selected value is: {selected}, since that is what the Value prop is on the select option"
+            | None -> ()
+        ]
+
+        Select.select [ ] [
+            select [
+                match model.SelectedOption with
+                | Some o -> Value o
+                | None -> ()
+                OnChange (fun x -> dispatch (SetDropDownOption x.target?value))
+            ] [
+                for row in model.DropDownOptions do
+                    option [ Value row ] [ str row.Description ]
+            ]
+        ]
     ]
-
-
